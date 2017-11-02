350fc6880041bc73fceddf975c08a65a91dc2a0e
Camden Ko <camdenko@gmail.com>
2017-11-02 18:27:55 -0500

    full wav functionality - needs debugging

--- sounds.py
+++ sounds.py
@@ -1,17 +1,34 @@
 # NOTE: For this coding exercise I will simply store the 3 seconds sound in memory, if ti were longer I would stream this from disk.
 
 import wave
+import struct
 from requester import request_to_random
 
 def create_wav_file(file_name):
   return wave.Wave_write(file_name)
 
 def create_sounds_arr(sample_rate, duration):
-  num_samples = duration * sample_rate / 1000.0
-  return request_to_random(num_samples, )
+  num_samples = int(duration * sample_rate / 1000.0)
+  return request_to_random(num_samples, -32767, 32767)
 
-def create_random_wav():
+def save_wav(sounds_arr, wav_file, sample_rate):
+  nchannels = 1
+  sampwidth = 2
+  nframes = len(sounds_arr)
+  comptype = 'NONE'
+  compname = 'not compressed'
+  wav_file.setparams((nchannels, sampwidth, sample_rate, nframes, comptype, compname))
+
+  for sample in sounds_arr:
+    wav_file.writeframes(struct.pack('h', sample))
+
+  wav_file.close()
+
+def create_random_wav(file_name):
   sample_rate = 44100.0
-  duration = 3000 #MS
-  audio = create_sounds(sample_rate, duration)
+  duration = 30 #MS
+  sounds_arr = create_sounds_arr(sample_rate, duration)
+  wav_file = create_wav_file(file_name)
+  save_wav(sounds_arr, wav_file, sample_rate)
+
 # save_file('as')
