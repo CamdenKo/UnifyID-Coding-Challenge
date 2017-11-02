1c0103a45ad2b3a2ee1006a314a8fb99a0c229af
Camden Ko <camdenko@gmail.com>
2017-11-02 18:21:00 -0500

    renamed variables to make them more pythony

--- requester.py
+++ requester.py
@@ -2,6 +2,6 @@ import secrets
 from jsonrpcclient.http_client import HTTPClient
 
 client = HTTPClient('https://api.random.org/json-rpc/1/invoke')
-def request_to_random(n, minRGB, maxRGB):
-  response = client.request('generateIntegers', apiKey=secrets.randomAPI, n=n, min=minRGB, max=maxRGB)
+def request_to_random(n, min_val, max_val):
+  response = client.request('generateIntegers', apiKey=secrets.randomAPI, n=n, min=min_val, max=max_val)
   return response['random']['data']
--- sounds.py
+++ sounds.py
@@ -1,12 +1,17 @@
 # NOTE: For this coding exercise I will simply store the 3 seconds sound in memory, if ti were longer I would stream this from disk.
 
 import wave
+from requester import request_to_random
 
-audio = []
-sample_rate = 44100.0
+def create_wav_file(file_name):
+  return wave.Wave_write(file_name)
 
-def save_file(file_name):
-  wave.open('audio')
+def create_sounds_arr(sample_rate, duration):
+  num_samples = duration * sample_rate / 1000.0
+  return request_to_random(num_samples, )
 
+def create_random_wav():
+  sample_rate = 44100.0
+  duration = 3000 #MS
+  audio = create_sounds(sample_rate, duration)
 # save_file('as')
-wave.Wave_write('audio')
