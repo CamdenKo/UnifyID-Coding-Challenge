48aa197c06be9bc7c8db71c18a0550eddd1c8717
Camden Ko <camdenko@gmail.com>
2017-11-02 18:02:04 -0500

    randomized image saves to randImg.png

--- requester.py
+++ requester.py
@@ -1,6 +1,7 @@
 import requests
 import secrets
 import json
+import os
 from PIL import Image
 # from PIL import RGB
 from jsonrpcclient.http_client import HTTPClient
@@ -11,10 +12,23 @@ def request_to_random(n, min, max):
   return response['random']['data']
 
 def create_image(width, height):
-  img = Image.new('RGB', (width, height), 'black')
+  return Image.new('RGB', (width, height), 'black')
+
+def set_pixels(img, rgb_arr):
+  counter = 0
   pixels = img.load()
   for col in range(img.size[0]):
     for row in range(img.size[1]):
-      pixels[col, row] = (255, 0, 0)
+      pixels[col, row] = (rgb_arr[counter], rgb_arr[counter + 1], rgb_arr[counter + 2])
+      counter = counter + 3
+
+def create_random_image():
+  width = 10 #128
+  height = 10
+  rgb_arr = request_to_random(width * height * 3, 0, 255)
+  img = create_image(width, height)
+  set_pixels(img, rgb_arr)
   img.show()
-create_image(100, 100)
+  img.save('randomImg', 'PNG')
+
+create_random_image()
