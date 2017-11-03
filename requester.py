1a24f14f2d288c6904aba4cf6c6bb1005fd8b9b6
Camden Ko <camdenko@gmail.com>
2017-11-02 19:21:14 -0500

    requester documentation

--- requester.py
+++ requester.py
@@ -1,7 +1,10 @@
+"""Requester for Random.org API, must save Random.org API key as randomAPI in secrets.py"""
+
 import secrets
 from jsonrpcclient.http_client import HTTPClient
 
 client = HTTPClient('https://api.random.org/json-rpc/1/invoke')
 def request_to_random(n, min_val, max_val):
+  """n - the number of numbers (int), min_val (int), max_val (int), returns an array of ints with count n, [min_val, max_val]"""
   response = client.request('generateIntegers', apiKey=secrets.randomAPI, n=n, min=min_val, max=max_val)
   return response['random']['data']
