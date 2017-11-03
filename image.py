c760941b1ff20000a70364ec53c0eda9e36e5c0d
Camden Ko <camdenko@gmail.com>
2017-11-02 19:24:17 -0500

    images.py

--- image.py
+++ image.py
@@ -1,11 +1,15 @@
+"""Resources needed to create a random 128 * 128 BMP"""
+
 from PIL import Image
 
 from requester import request_to_random
 
 def create_image(width, height):
+  """width(int), height(int), returns an instance of Image"""
   return Image.new('RGB', (width, height), 'black')
 
 def set_pixels(img, rgb_arr, cluster_size):
+  """img(Image instance), rgb_arr(array of ints, each one being r,g, or b), cluster_size(int) sets all the pixels of the image according to rgb_arr with each rgb taking up cluster_size x cluster_size"""
   pixels = img.load()
   counter = 0
   col = 0
@@ -20,6 +24,7 @@ def set_pixels(img, rgb_arr, cluster_size):
     col += cluster_size
 
 def set_cluster(vals, pixels, min_x, max_x, min_y, max_y):
+  """vals(tuple of ints representing RGB), pixels(pixels property of Image instance), min_x(int), max_x(int), min_y(int), max_y(int) sets cluster covered by min to max x to y to the vals set"""
   x = min_x
   y = min_y
   while x <= max_x:
@@ -30,6 +35,7 @@ def set_cluster(vals, pixels, min_x, max_x, min_y, max_y):
     x += 1
 
 def create_random_image(file_name):
+  """takes in a file_name(string) and saves a random bitmap to the file name"""
   width = 128
   height = 128
   cluster_size = 8
